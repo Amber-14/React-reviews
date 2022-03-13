100e73b2460a3ec6c328974efd80308c6ba8cce5
Amber-14 <amberyadav311@gmail.com>
2022-03-13 12:07:40 +0530

    reviews

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Reviews from './Reviews';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section className="section">
+      <div className="title">
+        <h2>
+        <span>/</span>Reviews
+        </h2>
+      </div>
+      <Reviews />
+    </section>
   );
 }
 
---
+++ src/Reviews.js
@@ -0,0 +1,80 @@
+import React, { useState, useEffect } from 'react'
+import { FaQuoteRight } from "react-icons/fa";
+import { FiChevronLeft, FiChevronRight } from "react-icons/fi"
+import data from './data';
+
+
+function Reviews() {
+    const [index, setIndex] = useState(0)
+    const [people, setPeople] = useState(data)
+
+    const prevSlide = () => {
+        setIndex((oldIndex) => {
+            let tempIndex = oldIndex - 1;
+            if (tempIndex < 0) {
+                tempIndex = people.length - 1
+            }
+            return tempIndex
+        })
+    }
+
+    const nextSlide = () => {
+        setIndex((oldIndex) => {
+            let tempIndex = oldIndex + 1;
+            if (tempIndex > people.length - 1) {
+                tempIndex = 0
+            }
+            return tempIndex
+        })
+    }
+    useEffect(() => {
+        // effect
+        let slider = setTimeout(()=>{
+          setIndex((oldIndex)=>{
+            let ind = oldIndex+1;
+            if(ind>people.length-1){
+              ind = 0
+            }
+            return ind
+          })
+          },3000)
+        return () => {
+          // cleanup
+          clearTimeout(slider)
+        };
+      }, [index,people.length]);
+
+    return (
+        <div className="section-center">
+            {
+                people.map((person, personIndex) => {
+
+                    const { id, name, title, image, quote } = person
+
+                    let position = 'nextSlide'
+
+                    if (personIndex === index) {
+                        position = 'activeSlide'
+                    }
+                    if (personIndex === index - 1 || (index === 0 && personIndex === people.length - 1)) {
+                        position = "lastSlide"
+                    }
+
+                    return (
+                        <article className={position} key={id}>
+                            <img src={image} alt={name} className='person-img' />
+                            <h4>{name}</h4>
+                            <p className="title">{title}</p>
+                            <p className="text">{quote}</p>
+                            <FaQuoteRight className="icon"></FaQuoteRight>
+                        </article>
+                    )
+                })
+            }
+            <button className="prev" onClick={prevSlide}><FiChevronLeft /></button>
+            <button className="next" onClick={nextSlide}><FiChevronRight /></button>
+        </div>
+    )
+}
+
+export default Reviews
